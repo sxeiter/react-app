4cc9a0076e017052ef031c5fb193b484ced049ec
Pavel <sxeiter@mail.ru>
2024-01-04 19:11:55 +0500

    динамические классы

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,51 @@
 import './App.css';
-import CardButton from './components/CardButton/CardButton';
-import JournalItem from './components/JournalItem/JournalItem';
 import LeftPanel from './components/layouts/leftPanel/LeftPanel';
 import Body from './components/layouts/Body/Body';
 import Header from './components/Header/Header';
 import JournalList from './components/JournalList/JournalList';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
+import Button from './components/Button/Button';
+import { useState } from 'react';
 
+
+const INITIAL_DATA = [
+	// {
+	// 	id: 1,
+	// 	title: 'Подготовка к обновлению курсов',
+	// 	text: 'Горные походы открывают удивительные природные ландшафты',
+	// 	date: new Date()
+	// },
+	// {
+	// 	id: 2,
+	// 	title: 'Поход в горы',
+	// 	text: 'Думал, что очень много времени',
+	// 	date: new Date()
+	// }
+];
 
 function App() {
-	const data = [
-		{
-			title: 'Подготовка к обновлению курсов',
-			text: 'Горные походы открывают удивительные природные ландшафты',
-			date: new Date()
-		},
-		{
-			title: 'Поход в горы',
-			text: 'Думал, что очень много времени',
-			date: new Date()
-		}
-	];
+	const [items, setItems] = useState(INITIAL_DATA);
 
-
-  
+	const addItem = item => {
+		setItems(oldItems => [...oldItems, {
+			text: item.text,
+			title: item.title,
+			date: new Date(item.date),
+			id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1
+		}]);
+	};
+ 
 	return (
 		<div className='app'>
 			<LeftPanel>
 				<Header />
 				<JournalAddButton/>
-				<JournalList>
-					<CardButton>
-						<JournalItem
-							title={data[0].title}
-							text={data[0].text}
-							data={data[0].date}
-						/>
-					</CardButton>
-					<CardButton>
-						<JournalItem
-							title={data[1].title}
-							text={data[1].text}
-							data={data[1].date}
-						/>
-					</CardButton>
-				</JournalList>
+				<JournalList items={items} />
 			</LeftPanel>
 			<Body>
-				<JournalForm />
+				<JournalForm onSubmit={addItem} />
 			</Body>
-			
-			
-			
 		</div>
 	);
 }
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,29 +1,51 @@
 import './JournalForm.css';
-import { useState } from 'react';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
-function JournalForm() {
-	const [inputData, setInputData] = useState('');
-
-	const inputChange = (event) => {
-		setInputData(event.target.value);	
-	};
+function JournalForm({onSubmit} ) {
+	const [formValidState, setFormValidState] = useState({
+		title: true,
+		post: true,
+		date: true
+	});
 
 	const addJournalItem = (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
-		console.log(formProps);
+		let isFormValid = true;
+		if (!formProps.title?.trim().length) {
+			setFormValidState(state => ({...state, title: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, title: true}));
+		}
+		if (!formProps.text?.trim().length) {
+			setFormValidState(state => ({...state, post: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, post: true}));
+		}
+		if (!formProps.date) {
+			setFormValidState(state => ({...state, date: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, date: true}));
+		}
+		if (!isFormValid) {
+			return;
+		}
+		onSubmit(formProps);
 	};
 
 	return(
 		<>
 			<form className='journal-form' onSubmit={addJournalItem}>
-				<input type='text' name='title'  />
-				<input type='date' name='date' />
-				<input type='text' name='tag' value={inputData} onChange={inputChange} />
-				<textarea name='post' id='' cols="30" rows="10"></textarea>
-				<Button text="Сохранить" onClick={() => {console.log('нажали');}}/>
+				<input type='text' name='title' className={`input ${formValidState.title ? '' : 'invalid'}`} />
+				<input type='date' name='date' className={`input ${formValidState.date ? '' : 'invalid'}`}/>
+				<input type='text' name='tag' />
+				<textarea name='text' id='' cols="30" rows="10" className={`input ${formValidState.post ? '' : 'invalid'}`}></textarea>
+				<Button text="Сохранить" />
 			</form>
 		</>
 	);
