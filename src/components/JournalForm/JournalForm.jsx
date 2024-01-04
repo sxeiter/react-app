705360cf22d6f2db2236653f6b194210d751cb03
Pavel <sxeiter@mail.ru>
2024-01-04 20:17:35 +0500

    добавил иконки, сделал стили у формы

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,6 +1,7 @@
-import './JournalForm.css';
+import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
 import { useState } from 'react';
+import cn from 'classnames';
 
 function JournalForm({onSubmit} ) {
 	const [formValidState, setFormValidState] = useState({
@@ -40,11 +41,33 @@ function JournalForm({onSubmit} ) {
 
 	return(
 		<>
-			<form className='journal-form' onSubmit={addJournalItem}>
-				<input type='text' name='title' className={`input ${formValidState.title ? '' : 'invalid'}`} />
-				<input type='date' name='date' className={`input ${formValidState.date ? '' : 'invalid'}`}/>
-				<input type='text' name='tag' />
-				<textarea name='text' id='' cols="30" rows="10" className={`input ${formValidState.post ? '' : 'invalid'}`}></textarea>
+			<form className={styles['journal-form']} onSubmit={addJournalItem}>
+				<div>
+					<input type='text' name='title' className={cn(styles['input-title'], {
+						[styles['invalid']]: !formValidState.title
+					})} />
+				</div>
+				<div className={styles['form-row']}>
+					<label htmlFor="date" className={styles['form-label']}>
+						<img src='/calendar.svg' alt='Иконка календаря' />
+						<span>Дата</span>
+					</label>
+					<input type='date' name='date' id='date' className={cn(styles['input'], {
+						[styles['invalid']]: !formValidState.date
+					})}/>
+				</div>
+				<div className={styles['form-row']}>
+					<label htmlFor="tag" className={styles['form-label']}>
+						<img src='/folder.svg' alt='Иконка папки' />
+						<span>Метки</span>
+					</label>
+					<input type='text' id='tag' name='tag' className={styles['input']} />
+				</div>
+				
+				
+				<textarea name='text' id='' cols="30" rows="10" className={cn(styles['input'], {
+					[styles['invalid']]: !formValidState.post
+				})}></textarea>
 				<Button text="Сохранить" />
 			</form>
 		</>
